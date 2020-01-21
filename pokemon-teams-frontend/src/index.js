70780b9dfdb8a107955c83f0183752f56287f225
learn <vocalise@gmail.com>
2020-01-20 21:26:59 -0800

    Updated js file to output button to add a pokemon and render pokemon list

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const mainElement = document.querySelector("main");
 
 function getTrainerData() {
     return fetch("http://localhost:3000/trainers")
@@ -8,13 +9,34 @@ function getTrainerData() {
 }
 
 function renderTrainer(trainer_object) {
-    let mainElement = document.querySelector("main");
-    let divCardElement = document.createElement("div")
 
+    let divCardElement = document.createElement("div")
+    divCardElement.setAttribute("id", trainer_object.id);
+    divCardElement.setAttribute("class", "card");
+    
+    let btnElement = document.createElement("button")
+    btnElement.textContent = "Add Pokemon";
+    divCardElement.append(btnElement);
+
+    let pokemonPElement = document.createElement("p")
+    divCardElement.append(pokemonPElement);
+
+    let listElement = document.createElement("ul")
+    divCardElement.append(listElement);
+
+    let pokemons = trainer_object.pokemons;
+    pokemons.forEach(pokemon => {
+        let liElement = document.createElement("li");
+        liElement.textContent = pokemon.nickname + " (" + pokemon.species + ")";
+        listElement.append(liElement);
+    })
+    mainElement.append(divCardElement)
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
 
+
+console.log(mainElement)
     console.log("page is loaded");
     getTrainerData().then(trainers => {
         trainers.forEach(trainer => {
